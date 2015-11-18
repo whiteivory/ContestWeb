af77e9b1afea498971589fd7aaf9e313485c4c12
whiteivory <529384530@qq.com>
2015-11-18 19:17:56 +0800

    上传细节修补完成
    对ajax上传文字内inline图片功能修补上传同名图片会覆盖的问题

--- module/Forum/src/Forum/Controller/PageController.php
+++ module/Forum/src/Forum/Controller/PageController.php
@@ -8,6 +8,9 @@ use Forum\Form\PageForm;
 use Application\Common\WAuthUtil;
 use Forum\Model\Page;
 use Zend\Debug\Debug;
+use Zend\Filter\BaseName;
+use Zend\Filter\File\Rename;
+use Application\Common\WBasePath;
 
 class PageController  extends AbstractActionController
 {
@@ -18,6 +21,9 @@ class PageController  extends AbstractActionController
         $this->pageService = $pageService;
     }
     
+    public function testajaxAction(){
+        
+    }
     public function indexAction()
     {
         return new ViewModel(array(
@@ -88,16 +94,25 @@ class PageController  extends AbstractActionController
      */
     public function addUpPicSerAction(){
         $path_for_route="data/postinlineimg/";//由于在apache配置文件里设置到了public
-        $path_for_frame="public/".$path_for_route;//实际存的时候存放的地址。
+        $path_for_frame=WBasePath::getBasePath()."/".$path_for_route;//实际存的时候存放的地址。
 
 //         if (file_exists($path. $_FILES["upload"]["name"]))
 //         {
 //              echo $_FILES["upload"]["name"] . " already exists please choose another image.";
 //         }
 //             $ran_path_for_route=$this->getservice()->getRandomizedname();
-             move_uploaded_file($_FILES["upload"]["tmp_name"],
-             $path_for_frame . $_FILES["upload"]["name"]);
-             echo "Stored in: " . $path_for_frame . $_FILES["upload"]["name"];
+//         $suffixfilter=new BaseName();
+//         $suffixname=$suffixfilter->filter($_FILES['upload']['name']);//upload name
+        $filter=new Rename(array(
+            "target"=>$path_for_frame. $_FILES["upload"]["name"],
+            "randomize"=>true,
+        ));
+        $filepath=$filter->filter($_FILES['upload']['tmp_name']);
+        $suffixfilter=new BaseName();
+        $suffixname=$suffixfilter->filter($filepath);
+//              move_uploaded_file($_FILES["upload"]["tmp_name"],
+//              $path_for_frame . $_FILES["upload"]["name"]);
+//              echo "Stored in: " . $path_for_frame . $_FILES["upload"]["name"];
              // Required: anonymous function reference number as explained above.
              $funcNum = $_GET['CKEditorFuncNum'] ;
              // Optional: instance name (might be used to load a specific configuration file or anything else).
@@ -106,7 +121,7 @@ class PageController  extends AbstractActionController
              $langCode = $_GET['langCode'] ;
              
              // Check the $_FILES array and save the file. Assign the correct path to a variable ($url).
-             $url = $path_for_route . $_FILES["upload"]["name"];
+             $url = $path_for_route . $suffixname;
              // Usually you will only assign something here if the file could not be uploaded.
              $message = '';
              echo "<script type='text/javascript'> window.parent.CKEDITOR.tools.callFunction($funcNum, '$url', '$message');</script>";
