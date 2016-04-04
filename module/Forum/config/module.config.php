e6f5b87c5598787a640b2d643e21680e35d77881
root <root@VM_27_48_centos.(none)>
2016-04-04 23:30:32 +0800

    li

--- module/Forum/config/module.config.php
+++ module/Forum/config/module.config.php
@@ -5,7 +5,7 @@ return array(
     'db' => array(
         'driver'         => 'Pdo',
         'username'       => 'root',  //edit this
-        'password'       => '',  //edit this
+        'password'       => 'fubupingci',  //edit this
         'dsn'            => 'mysql:dbname=contestweb;host=localhost',
         'driver_options' => array(
             \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
